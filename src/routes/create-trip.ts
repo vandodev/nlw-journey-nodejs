5a0d858aaf5fa8a24c977f505ae0667e5f77748e
vandodev <vandoaparecido@hotmail.com>
2024-07-15 11:16:17 -0300

    :airplane: :ship: :car: validando datas com dayjs

--- src/routes/create-trip.ts
+++ src/routes/create-trip.ts
@@ -2,6 +2,8 @@ import type { FastifyInstance } from 'fastify'
 import { z } from 'zod'
 import { ZodTypeProvider } from 'fastify-type-provider-zod'
 import { prisma } from '../lib/prisma'
+import dayjs from 'dayjs'
+import { error } from 'console'
 
 export async function createTrip(app: FastifyInstance) {
     app.withTypeProvider<ZodTypeProvider>().post('/trips', {
@@ -19,6 +21,14 @@ export async function createTrip(app: FastifyInstance) {
             starts_at,
             ends_at,
           } = request.body
+
+          if (dayjs(starts_at).isBefore(new Date())) {
+            throw new Error('Invalid trip start date.')
+          }
+    
+          if (dayjs(ends_at).isBefore(starts_at)) {
+            throw new Error('Invalid trip end date.')
+          }
 
           const trip = await prisma.trip.create({
             data:{
