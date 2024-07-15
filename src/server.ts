c538d51aa4231453dc301c60f270758049e9f518
vandodev <vandoaparecido@hotmail.com>
2024-07-15 08:02:31 -0300

    :airplane: :ship: :car: Exemplo de cadastrar e listar com prisma

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,26 @@
 import fastify from 'fastify'
+import {prisma} from './lib/prisma'
 
 const app = fastify()
+
+//http://localhost:3333/cadastrar
+app.get('/cadastrar', async () => {
+     await prisma.trip.create({
+        data:{
+            destination: 'Ribeirão Preto',
+            ends_at: new Date,
+            starts_at: new Date
+        }
+    })
+
+    return 'Registro cadastrado com sucesso'
+})
+
+http://localhost:3333/listar
+app.get('/listar', async () => {
+    const trips = await prisma.trip.findMany()
+    return trips
+})
 
 app.listen({ port:3333 }).then(() => {
     console.log('Server running!')
