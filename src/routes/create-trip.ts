f7da566c5370ddf5779a061d53a44c14aa645972
vandodev <vandoaparecido@hotmail.com>
2024-07-16 08:06:33 -0300

    :airplane: :ship: :car: Adicionando convidados

--- src/routes/create-trip.ts
+++ src/routes/create-trip.ts
@@ -15,6 +15,7 @@ export async function createTrip(app: FastifyInstance) {
                 ends_at: z.coerce.date(),
                 owner_name: z.string(),
                 owner_email: z.string().email(),
+                emails_to_invite: z.array(z.string().email()),
             })
         }
     }, async (request) =>{
@@ -24,7 +25,8 @@ export async function createTrip(app: FastifyInstance) {
           starts_at,
           ends_at,
           owner_name,
-          owner_email
+          owner_email,
+          emails_to_invite
       } = request.body
 
           if (dayjs(starts_at).isBefore(new Date())) {
@@ -41,11 +43,18 @@ export async function createTrip(app: FastifyInstance) {
                 starts_at,
                 ends_at,
                 participants:{
-                  create:{
-                    name: owner_name,
-                    email: owner_email,
-                    is_owner:true,
-                    is_confirmed:true,
+                  createMany:{
+                    data:[                      
+                      { 
+                        name: owner_name,
+                        email: owner_email,
+                        is_owner:true,
+                        is_confirmed:true,
+                      },
+                        ...emails_to_invite.map((email) => {
+                          return { email }
+                        })
+                    ]
                   }
                 }
             }
