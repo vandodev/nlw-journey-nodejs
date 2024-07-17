845255ebab6bc408fbbab2c45863e208fd5ba1f8
vandodev <vandoaparecido@hotmail.com>
2024-07-17 07:37:20 -0300

    :airplane: :ship: :car: Estrutura da rota de confirmar viagem

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import fastify from 'fastify'
 import { createTrip } from './routes/create-trip'
+import { confirmTrip } from './routes/confirm-trip'
 import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod'
 import cors from '@fastify/cors'
 
@@ -10,6 +11,7 @@ app.register(cors, {
 })
 
 app.register(createTrip)
+app.register(confirmTrip)
 
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
