ce2f0bf8071121568499240a586d483524daeb9b
vandodev <vandoaparecido@hotmail.com>
2024-07-17 07:56:28 -0300

    :airplane: :ship: :car: Confirmando a viagem

--- src/routes/confirm-trip.ts
+++ src/routes/confirm-trip.ts
@@ -1,6 +1,7 @@
 import type { FastifyInstance } from 'fastify'
 import type { ZodTypeProvider } from 'fastify-type-provider-zod'
 import { z } from 'zod'
+import { prisma } from '../lib/prisma'
 
 export async function confirmTrip(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().get(
@@ -12,7 +13,28 @@ export async function confirmTrip(app: FastifyInstance) {
         }),
       },
     },
-    async (request) => {
+    async (request, reply) => {
+        const { tripId } = request.params
+
+        const trip = await prisma.trip.findUnique({
+            where: {
+                id: tripId,
+            },
+        })
+
+        if (!trip) {
+            throw new Error('Trip not found.')
+        }
+
+        if (trip.is_confirmed) {
+            return reply.redirect(`http://localhost:3333/trips/${tripId}`)
+        }
+
+        await prisma.trip.update({
+            where: { id: tripId },
+            data: { is_confirmed: true },
+        })
+
       return {tripId: request.params}
     },
   )
