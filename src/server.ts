7b16166c959e912120040390d7d8cbae019fe420
vandodev <vandoaparecido@hotmail.com>
2024-07-18 09:16:38 -0300

    :airplane: :ship: :car: Buscando por atividades

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import fastify from 'fastify'
-import { createTrip } from './routes/create-trip'
-import { confirmTrip } from './routes/confirm-trip'
-import { confirmParticipants } from './routes/confirm-participant'
 import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod'
 import cors from '@fastify/cors'
+
+import { confirmTrip } from './routes/confirm-trip'
+import { confirmParticipants } from './routes/confirm-participant'
+import { createTrip } from './routes/create-trip'
 import { createActivity } from './routes/create-activity'
+import { getActivities } from './routes/get-activities'
 
 const app = fastify()
 
@@ -19,6 +21,7 @@ app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
 app.register(confirmParticipants)
 app.register(createActivity)
+app.register(getActivities)
 
 app.listen({ port:3333 }).then(() => {
     console.log('Server running!')
