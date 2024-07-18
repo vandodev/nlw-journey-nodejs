b4bb2d78f1b5a3aa8aa6403f4ab78deb344a59a7
vandodev <vandoaparecido@hotmail.com>
2024-07-18 09:41:09 -0300

    :airplane: :ship: :car: Rota de criação de links

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,7 @@ import { confirmParticipants } from './routes/confirm-participant'
 import { createTrip } from './routes/create-trip'
 import { createActivity } from './routes/create-activity'
 import { getActivities } from './routes/get-activities'
+import { createLink } from './routes/create-link'
 
 const app = fastify()
 
@@ -22,6 +23,8 @@ app.setSerializerCompiler(serializerCompiler)
 app.register(confirmParticipants)
 app.register(createActivity)
 app.register(getActivities)
+app.register(createLink)
+
 
 app.listen({ port:3333 }).then(() => {
     console.log('Server running!')
